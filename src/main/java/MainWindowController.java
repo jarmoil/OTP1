2aeb18a384bc4b11172ae461c21a755cd11d2ec2
jarmo <jarmoil@metropolia.fi>
2025-09-12 17:08:58 +0300

    implement stats view in the application and made teacher / student Sets

--- src/main/java/MainWindowController.java
+++ src/main/java/MainWindowController.java
@@ -2,12 +2,18 @@ import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
 
 
 
 public class MainWindowController{
     @FXML private Pane titlePane;
     @FXML private ImageView btnMinimize, btnClose;
+    @FXML private Pane btnStats;
+    @FXML private Label btnStatsLabel;
+
 
     private double x, y;
 
@@ -23,5 +29,35 @@ public class MainWindowController{
 
         btnClose.setOnMouseClicked(mouseEvent -> stage.close());
         btnMinimize.setOnMouseClicked(mouseEvent -> stage.setIconified(true));
+        btnStats.setOnMouseClicked(mouseEvent -> {
+            if (btnStatsLabel != null && "Main".equals(btnStatsLabel.getText())) {
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/window.fxml"));
+                    Parent mainRoot = loader.load();
+                    MainWindowController mainController = loader.getController();
+                    stage.getScene().setRoot(mainRoot);
+                    mainController.init(stage);
+                    mainController.setbtnStatsLabel("Stats");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/stats.fxml"));
+                    Parent statsRoot = loader.load();
+                    MainWindowController statsController = loader.getController();
+                    stage.getScene().setRoot(statsRoot);
+                    statsController.init(stage);
+                    statsController.setbtnStatsLabel("Main");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    public void setbtnStatsLabel(String text){
+        if (btnStatsLabel != null) {
+            btnStatsLabel.setText(text);
+        }
     }
 }
