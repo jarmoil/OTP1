d17c07c9af0c72b14ac1ccefc7b198089f2c949d
xTheKn1ght <aaron.lyhtinen@gmail.com>
2025-09-12 15:43:40 +0300

    ConnectDB done and works with db.properties

---
+++ src/main/java/database/ConnectDB.java
@@ -0,0 +1,30 @@
+package database;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class ConnectDB {
+    private static final Properties props = new Properties();
+
+    static {
+        try (InputStream input = ConnectDB.class.getClassLoader().getResourceAsStream("db.properties")) {
+            if (input == null) {
+                throw new RuntimeException("Could not find db.properties in resources");
+            }
+            props.load(input);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load database properties", e);
+        }
+    }
+
+    public static Connection getConnection() throws Exception {
+        String url = "jdbc:mariadb://" + props.getProperty("DB_HOST") + ":" +
+                props.getProperty("DB_PORT") + "/" + props.getProperty("DB_NAME");
+        String user = props.getProperty("DB_USER");
+        String password = props.getProperty("DB_PASSWORD");
+
+        return DriverManager.getConnection(url, user, password);
+    }
+}
