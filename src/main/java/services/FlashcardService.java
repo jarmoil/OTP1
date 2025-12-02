5d24bd31818780de072de7c11e47f023685fb6fe
ottopar <ottoparnanen1@gmail.com>
2025-12-02 13:25:57 +0200

    Refactor FlashcardService to use dedicated exceptions

--- src/main/java/services/FlashcardService.java
+++ src/main/java/services/FlashcardService.java
@@ -1,6 +1,7 @@
 package services;
 
 import dao.IFlashcardDao;
+import exceptions.DataOperationException;
 import models.Flashcard;
 import utils.validation.FlashcardData;
 import utils.validation.Validator;
@@ -18,13 +19,13 @@ public class FlashcardService {
     }
 
     // Retrieve all flashcards for a given flashcard set ID
-    public List<Flashcard> getFlashcardsBySetId(int setId) throws Exception {
+    public List<Flashcard> getFlashcardsBySetId(int setId) throws DataOperationException {
         return flashcardDao.getFlashcardsBySetId(setId);
     }
 
     // Create a new flashcard in the database using the DAO
     public boolean createFlashcard(int setId, String question, String answer,
-                                   String choiceA, String choiceB, String choiceC) throws Exception {
+                                   String choiceA, String choiceB, String choiceC) throws DataOperationException {
         FlashcardData data = new FlashcardData(question, answer, choiceA, choiceB, choiceC);
         validator.validate(data);
         return flashcardDao.createFlashcard(setId, question, answer, choiceA, choiceB, choiceC);
@@ -32,18 +33,18 @@ public class FlashcardService {
 
     // Update an existing flashcard in the database using the DAO
     public boolean updateFlashcard(int flashcardId, String question, String answer,
-                                   String choiceA, String choiceB, String choiceC) throws Exception {
+                                   String choiceA, String choiceB, String choiceC) throws DataOperationException {
         FlashcardData data = new FlashcardData(question, answer, choiceA, choiceB, choiceC);
         validator.validate(data);
         return flashcardDao.updateFlashcard(flashcardId, question, answer, choiceA, choiceB, choiceC);
     }
 
     // Delete a flashcard from the database using the DAO
-    public boolean deleteFlashcard(int flashcardId) throws Exception {
+    public boolean deleteFlashcard(int flashcardId) throws DataOperationException {
         return flashcardDao.deleteFlashcard(flashcardId);
     }
 
-    public void updateFlashcardStats(int flashcardId, boolean isCorrect) throws Exception {
+    public void updateFlashcardStats(int flashcardId, boolean isCorrect) throws DataOperationException {
         flashcardDao.updateFlashcardStats(flashcardId, isCorrect);
     }
 
